6e4aaf6da46c32d24f875046f248734fe1d6b673
Damian Diez <d.a.diez87@gmail.com>
2022-07-12 09:55:24 -0300

    first commit

---
+++ TextEncoder/Encoder.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace TextEncoder
+{
+    public static class Encoder
+    {
+        public static void Encode(string text)
+        {
+            text=text.Replace(" ", "");
+            var L = text.Length;
+            var x = Math.Sqrt(L);
+            int ceiling = (int)Math.Ceiling(x);
+            int floor= (int)Math.Floor(x);
+            for (var i = 0; i < ceiling; i++) {
+                for (var j = 0; j <= floor; j++) {
+                    int index = ceiling * j + i;
+                    if (index < L)
+                        Console.Write(text[index]);
+                }
+                Console.Write(" ");
+            }
+        }
+    }
+}
---
+++ TextEncoder/Program.cs
@@ -0,0 +1,12 @@
+﻿using TextEncoder;
+
+Encoder.Encode("we the people of the united states in order to form a more perfect union etc");
+Console.WriteLine();
+Encoder.Encode("cheating is not allowed");
+Console.WriteLine();
+Encoder.Encode("the rocks");
+Console.WriteLine();
+Encoder.Encode("");
+Console.WriteLine();
+
+Console.ReadLine();
